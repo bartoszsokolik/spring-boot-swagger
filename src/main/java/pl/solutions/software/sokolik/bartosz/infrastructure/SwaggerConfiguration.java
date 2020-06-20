d6fe49dd40543a5196725670b2e1149cd0ffc9c0
Bartosz Sokolik <bsokolik@enzode.com>
2020-06-20 16:15:41 +0200

    Add alternative swagger config

--- src/main/java/pl/solutions/software/sokolik/bartosz/infrastructure/SwaggerConfiguration.java
+++ src/main/java/pl/solutions/software/sokolik/bartosz/infrastructure/SwaggerConfiguration.java
@@ -1,43 +1,77 @@
 package pl.solutions.software.sokolik.bartosz.infrastructure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    @Value("${swagger.proxy.path}")
+    private String basePath;
+
+    @Value("${swagger.base.package}")
+    private String basePackage;
+
     @Bean
-    Docket api() {
+    public Docket docketSwaggerApi(ServletContext servletContext) {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(getApiInfo())
+                .pathProvider(new RelativePathProvider(servletContext) {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return basePath;
+                    }
+                })
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Demo with Swagger")
-                .description("Spring Demo with Swagger")
-                .contact(getContact())
-                .license("Apache 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                .version("1.0")
-                .build();
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                                           .scopeSeparator(",")
+                                           .additionalQueryStringParams(null)
+                                           .useBasicAuthenticationWithAccessCodeGrant(false)
+                                           .build();
     }
 
-    private Contact getContact() {
-        return new Contact("Bartosz Sokolik", "example url", "example@testdomain.com");
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                              .securityReferences(defaultAuth())
+                              .forPaths(PathSelectors.any())
+                              .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        return Collections.singletonList(
+                new SecurityReference("apiKey", new AuthorizationScope[]{authorizationScope}));
     }
 
 }
